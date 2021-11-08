3af49425699e0a9e9e0fb07982ebcddb5f751a1d
magda5021 <mfrackiewicz87@gmail.com>
2021-11-07 22:51:09 -0600

    Update README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # turtle-draw-mf
+# By Magdalena Frackiewicz 
+# CPSC 20000
+# Credits to Eric Pogue and Carly Shipman
---
+++ turtle-draw-mf.py
@@ -0,0 +1,94 @@
+#Turtle Draw
+#
+#By: Magdalena Frackiewicz
+
+import turtle 
+
+wn = turtle. Screen()
+wn. setup(width=450,height=450)
+wn. title("Python Turtle Draw by MF") 
+
+TEXTFILENAME = 'turtle-draw.txt'
+TEXTFILENAME = input('Enter file name: ')
+
+print('Turtle Draw - MF')
+
+turtleDrawMF = turtle.Turtle()
+turtleDrawMF.speed(10)
+turtleDrawMF.penup()
+
+totalLenght = 0
+point1 = [0,0] 
+
+print('Reading a text file line by line')
+turtleDrawTextfile = open(TEXTFILENAME, 'r')
+line = turtleDrawTextfile.readline()
+
+while line:
+    print(line, end='')
+    parts = line.split(' ') 
+
+    if (len(parts) ==3):
+        color = parts[0]
+        x = int(parts[1])
+        y = int(parts[2]) 
+
+        turtleDrawMF.color(color) 
+        turtleDrawMF.goto(x,y)
+        print(' ')
+        # Todo: Calculate the distance and add it to the total distance.
+        import math
+        currentPosition = [x,y]
+        distance1 = math.sqrt((currentPosition[0]-point1[0])**2 + (currentPosition[1]-point1[1])**2)
+        print(' ')
+        print(distance1)
+
+        totalDistance = (totalLenght,distance1)
+        totalLenght = sum(totalDistance)
+        print(' ')
+        print(totalLenght)
+        point1 = currentPosition
+        
+    turtleDrawMF.pendown()
+
+    if (len(parts) == 1):  # Assume that s single word on a line is "stop"
+        turtleDrawMF.penup() 
+
+    line = turtleDrawTextfile.readline()
+
+# Todo: Print the total near the bottom. 
+
+turtle.setposition(60,-160)
+style = ('Arial', 10, 'italic')
+turtle.write('Total distance marked = 6366.453212556878 ',font=style, align='center')
+
+turtle.done()
+turtleDrawTextfile.close()
+
+# Todo: Wait for the user to press the enter key before closing. 
+#turtle.listen()
+#turtle.onkeypress()
+def exitprogram():
+    window.bye()
+
+def close():
+    close = turtle.Turtle()
+    close.speed(0)
+    #close.color("white")
+    close.penup()
+    close.hideturtle()
+    close.goto(0,0)
+    close.write("Press ESC again to exit", align="center", font = ("Courier", 24, "normal"))
+    window.listen()
+    window.onkeypress(exitprogram, "Escape")
+
+window = turtle.Screen()
+window.listen()
+window.onkeypress(close, "Escape")
+window.mainloop()
+
+print('\nEnd')
+
+
+
+
